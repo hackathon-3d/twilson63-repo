afeb0590196cc51ebd5b15a95d0a45acdff2808a
Tom Wilson <tom@jackhq.com>
2013-08-24 00:31:35 -0400

    added checkins

--- public/js/app.js
+++ public/js/app.js
@@ -15685,15 +15685,46 @@ angular.module("firebase").factory("angularFireAuth", [
     window.auth = new FirebaseSimpleLogin(ref, function(err, user) { 
      if (err) { console.log(err); }
      if (user) {
+       angularFire('https://zpn.firebaseio.com/checkins/' + user.id, $scope, 'checkin', {});
        angularFire('https://zpn.firebaseio.com/users/' + user.id, $scope, 'user', user);
-       localStorage.setItem('user', user.id);
-       $scope.page = 'checkin';
+       navigator.geolocation.getCurrentPosition(function(position) {
+         $scope.$apply(function() {
+           $scope.checkin = {
+              lat: position.coords.latitude,
+              lng: position.coords.longitude,
+              stmp: new Date()
+            };
+           console.log($scope.checkin);
+           $scope.page = 'main';
+         });
+       }, function(err) {
+          alert(err);
+       });
+       
      }
    });
 
    $scope.login = function(source) {
      auth.login(source);
    };
+
+   $scope.rank = function() {
+     $scope.page = 'rank';
+   };
+
+   $scope.grid = function() {
+     $scope.page = 'main';
+   };
+
+   // $scope.checkin = function() {
+   //   navigator.geolocation.getCurrentPosition(function(position) {
+   //     var userId = localStorage.getItem('user');       localStorage.setItem('user', user.id);
+   //     angularFire('https://zpn.firebaseio.com/checkins/' + userId, $scope, 'checkin', position);
+   //     $scope.page = 'main';
+   //   }, function(err) {
+   //     alert(err);
+   //   });
+   // };
 
   });;angular.module('App')
   .controller('SignupCtrl', function($scope, angularFire) {

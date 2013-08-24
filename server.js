1f11b8897e6773b5339d89f8975d03d36d47ab36
Tom Wilson <tom@jackhq.com>
2013-08-24 05:14:02 -0400

    added endpoint

--- server.js
+++ server.js
@@ -5,3 +5,13 @@ app.use(express.static('public'));
 
 app.listen(process.env.PORT || 3000);
 
+var Firebase = require('firebase');
+var dataRef = new Firebase('https://zpn.firebaseIO.com/checkins');
+dataRef.on('child_changed', function(snapshot) {
+  console.log(snapshot.va());
+});
+
+var dataRef = new Firebase('https://zpn.firebaseIO.com/checkins');
+dataRef.on('child_added', function(snapshot) {
+  console.log(snapshot.name());
+});
